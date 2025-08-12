ddd1c412457da985e9432ced08d54d7e4b741dfd
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-08-12 16:46:18 +0530

    Added custom fonts, initialized storybook, initialized react-router-dom and set up the routes

---
+++ frontend/components/Heading.tsx
@@ -0,0 +1,7 @@
+export function Heading({label} : {label: string}){
+    return(
+        <div className=" font-headland text-2xl font-bold text-gray-500 ">
+            {label}
+        </div>
+    )
+}

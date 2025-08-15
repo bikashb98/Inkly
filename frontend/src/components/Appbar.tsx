a9c8a360c2f596e1ca8ac695ad51a42e9b814672
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-08-15 19:41:00 +0530

    Fixed few styling issues of the App bar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -10,12 +10,12 @@ export function Appbar({ authorName }: AppbarProps) {
                 {/* Left side - Hamburger and Logo */}
                 <div className='flex items-center gap-4'>
                     <button className="p-1 hover:bg-slate-100 rounded transition-colors w-8 h-8 flex items-center justify-center">
-                        <Bars3Icon className='w-6 h-6 text-slate-600 scale-130' />
+                        <Bars3Icon className='w-6 h-6 cursor-pointer text-slate-600 scale-130' />
                     </button>
                     <div className="flex items-center max-w-[100px] max-h-[20px] ">
-                        <img src={Inklylogo} alt="Inkly Logo" className='object-contain' />
+                        <img src={Inklylogo} alt="Inkly Logo" className='object-contain cursor-pointer' />
                     </div>
-                    <div className='bg-slate-100 border flex items-center h-10 rounded-full px-3 gap-2'>
+                    <div className='bg-slate-100  flex items-center h-10 rounded-full px-3 gap-2'>
                         <MagnifyingGlassIcon className='w-5 h-5 text-slate-400' />
                         <input className='bg-transparent outline-none  flex-1' type='text' placeholder='Search'/>
                     </div>
@@ -23,12 +23,12 @@ export function Appbar({ authorName }: AppbarProps) {
                 
                 {/* Right side - Write button and Profile */}
                 <div className='flex items-center gap-8'>
-                    <button className="flex items-center gap-2 px-3 py-2 hover:bg-slate-100 rounded transition-colors">
-                        <PencilSquareIcon className='w-6 h-6 text-slate-600' />
-                        <span className="text-slate-600 font-medium">Write</span>
+                    <button className="group flex items-center gap-2 px-3 py-2 rounded transition-colors cursor-pointer">
+                        <PencilSquareIcon className='w-6 h-6 text-slate-600 group-hover:text-slate-800 transition-colors' />
+                        <span className="text-slate-600 font-medium group-hover:text-slate-800 transition-colors">Write</span>
                     </button>
                     <button >
-                        <BellIcon className='w-6 h-6 text-slate-600 hover:text-slate-800'  />
+                        <BellIcon className='w-6 h-6 text-slate-600 hover:text-slate-800 cursor-pointer'  />
                     </button>
                     <div className="bg-slate-200 h-10 w-10 rounded-full flex items-center justify-center font-medium text-lg text-slate-600 hover:bg-slate-300 transition-colors cursor-pointer">
                         {authorName.slice(0, 1).toUpperCase()}

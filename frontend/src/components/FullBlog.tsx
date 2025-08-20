d7caeb1106ec0218df13c196d43107f26e91fb88
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-08-20 22:38:47 +0530

    Removed the full blog page

--- frontend/src/components/FullBlog.tsx
+++
@@ -1,8 +0,0 @@
-export function FullBlog() {
-    return(
-        <div className="flex  ">
-                <div className="flex mx-auto justify-center ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nostrum ea asperiores necessitatibus quod veritatis provident natus earum quisquam magnam fuga iste nobis minima non, commodi tenetur aperiam voluptates ab.</div>
-                
-        </div>
-    )
-}

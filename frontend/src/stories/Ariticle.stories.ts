93884f4c58c84356fac37931de1681087efb894c
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-08-24 12:15:30 +0530

    Wrote story for the article component

--- frontend/src/stories/Ariticle.stories.ts
+++ frontend/src/stories/Ariticle.stories.ts
@@ -2,6 +2,17 @@ import type {Meta, StoryObj} from '@storybook/react-vite'
 import { Article } from '../components/Article'
 
 
-const meta {
+
+const meta = {
     title: 'Components/Article',
-}
+    component: Article, 
+    tags: ['autodocs']
+    
+} satisfies Meta<typeof Article>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+
+export const Primary: Story = {}
